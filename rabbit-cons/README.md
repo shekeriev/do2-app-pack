ec3be0bf21bef9dd71cb565ae42370542086b5d1
Dimitar Zahariev <dimitar@zahariev.pro>
2023-01-28 15:16:51 +0200

    Newer Python version plus a few other cosmetic changes

--- rabbit-cons/README.md
+++ rabbit-cons/README.md
@@ -22,3 +22,19 @@ docker container run [-d|-it] [--name fancyname] [--net appnet] [-e VAR1=VALUE1 
 # Example
 docker container run -d --name cons --net appnet -e BROKER=rabbitmq -e BROKERPORT=5672 -e EXCHANGE=demo -e TOPICS='demo.*' shekeriev/rabbit-cons
 ```
+
+## Sample output
+
+Here is a sample output:
+
+```text
+ [x] (R) Consumer started. Press Ctrl+C to stop.
+ [x] (R) Subscribed for # on rabbitmq:5672. Listening ...
+ [x] (R) 2023-01-28 13:04:57 / 'ram.info': b'info: ram load is 34'
+ [x] (R) 2023-01-28 13:05:02 / 'cpu.warn': b'warn: cpu load is 73'
+ [x] (R) 2023-01-28 13:05:20 / 'cpu.warn': b'warn: cpu load is 74'
+```
+
+## Source code
+
+The latest version of the source code can be obtained from here: <https://github.com/shekeriev/do2-app-pack/tree/main/rabbit-cons>
--- rabbit-cons/code/app.py
+++ rabbit-cons/code/app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import pika
 import os
+from time import strftime
 
 brokerhost = os.getenv('BROKER', 'localhost')
 brokerport = os.getenv('BROKERPORT', 5672)
 exchange = os.getenv('EXCHANGE', 'demo')
 topics = os.getenv('TOPICS', '#')
 
-print(' [*] Consumer started. Subscribed for ' + str(topics) + ' on ' + brokerhost + ':' + str(brokerport) + '. Press Ctrl+C to stop.')
+print(' [x] (R) Consumer started. Press Ctrl+C to stop.')
 
 def callback(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body))
+    print(" [x] (R) " + strftime("%Y-%m-%d %H:%M:%S") + " / %r: %r" % (method.routing_key, body))
 
 try:
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=brokerhost, port=brokerport))
@@ -19,6 +20,7 @@ try:
     result = channel.queue_declare('', exclusive=True)
     queue_name = result.method.queue
     channel.queue_bind(exchange=exchange, queue=queue_name, routing_key=topics)
+    print(' [x] (R) Subscribed for ' + str(topics) + ' on ' + brokerhost + ':' + str(brokerport) + '. Listening ...')
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
 except Exception as ex:
@@ -29,4 +31,4 @@ finally:
     if connection is not None:
         connection.close()
 
-print('  [*] ... closed.')
+print('  [x] (R) ... closed.')
