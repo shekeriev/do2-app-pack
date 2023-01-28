b7067d613865465a20c19de5325e3cda194c835e
Dimitar Zahariev <dimitar@zahariev.pro>
2023-01-28 13:58:05 +0200

    Newer Python version plus a few other cosmetic changes

--- kafka-cons/README.md
+++ kafka-cons/README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-Subcribes for messages on a topic to a broker
+Subcribes for events on a **topic** to a **broker**. Both, the topic and broker, can be managed via environment variables
 
 ## Available variables
 
@@ -20,3 +20,19 @@ docker container run [-d|-it] [--name fancyname] [-e VAR1=VALUE1 -e ...] shekeri
 # Example
 docker container run -d --name cons -e BROKER=kafka:9092 -e TOPIC=demo-topic shekeriev/kafka-cons
 ```
+
+## Sample output
+
+Here is a sample output:
+
+```text
+ [x] (K) Consumer started. Press Ctrl+C to stop ...
+ [x] (K) Subscribed for prep on kafka:9092. Listening ...
+ [x] (K) 2023-01-28 11:37:50 / 276 / b'warn: ram load is 53'
+ [x] (K) 2023-01-28 11:37:59 / 277 / b'info: cpu load is 40'
+ [x] (K) 2023-01-28 11:38:11 / 278 / b'info: cpu load is 47'
+```
+
+## Source code
+
+The latest version of the source code can be obtained from here: <https://github.com/shekeriev/do2-app-pack/tree/main/kafka-cons>
--- kafka-cons/code/app.py
+++ kafka-cons/code/app.py
@@ -1,16 +1,18 @@
 from kafka import KafkaConsumer
 import os
+from datetime import datetime
 
 brokerhost = os.getenv('BROKER', "localhost:9092")
 topic = os.getenv('TOPIC', 'demo')
 
-print(' [*] Consumer started. Subscribed for ' + topic + ' on ' + brokerhost + '. Press Ctrl+C to stop.')
+print(' [x] (K) Consumer started. Press Ctrl+C to stop ...')
 
 try:
     consumer = KafkaConsumer(bootstrap_servers=[brokerhost])
     consumer.subscribe(topic)
-    for message in consumer:
-        print(' [x] ' + str(message))
+    print(' [x] (K) Subscribed for ' + topic + ' on ' + brokerhost + '. Listening ...')
+    for evnt in consumer:
+        print(' [x] (K) ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' / ' + str(evnt.offset) + ' / ' + str(evnt.value))
 except Exception as ex:
     print(str(ex))
 except KeyboardInterrupt:
@@ -19,4 +21,4 @@ finally:
     if consumer is not None:
         consumer.close()
 
-print(' [x] ... closed.')
+print(' [x] (K) ... closed.')
